2b5040bb48e323b5a3f9da8fb3b61c3824eb02e3
Brayan Rivera Cepeda <riveracepedabrayan@gmail.com>
2022-08-09 01:14:29 -0500

    Ready for QA

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { Prisma, PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async getHello(): Promise<string> {
+    try {
+      let dbTest = await prisma.user.findUnique({
+        where: {
+          username: 'JohnDoe',
+        },
+      });
+      if (!dbTest) {
+        dbTest = await prisma.user.create({
+          data: {
+            role: 'admin',
+            first_name: 'John',
+            last_name: 'Doe',
+            email: 'John2Doe2@mail.com',
+            username: 'JohnDoe',
+            password: '123456',
+            dni: '123456789',
+            user_state: 'active',
+          },
+        });
+      }
+      return 'Hello World for you, test admin! ' + dbTest.username;
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
   }
 }
