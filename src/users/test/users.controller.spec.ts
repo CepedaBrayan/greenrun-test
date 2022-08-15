cff119b814a3f68d27061039391ca2296dc9f0c8
Brayan Rivera Cepeda <riveracepedabrayan@gmail.com>
2022-08-14 19:21:22 -0500

    testing and pipeline ready for create user admin test

--- src/users/test/users.controller.spec.ts
+++ src/users/test/users.controller.spec.ts
@@ -2,7 +2,8 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from '../users.controller';
 import { UsersService } from '../users.service';
 import { CreateUserClientDto } from '../dto/create-user-client.dto';
-import { BadRequestException } from '@nestjs/common';
+import { CreateUserAdminDto } from '../dto/create-user-admin.dto';
+import { BadRequestException, UnauthorizedException } from '@nestjs/common';
 
 //working on with real database - and after delete
 import { PrismaClient } from '@prisma/client';
@@ -18,6 +19,17 @@ const userClientExample: CreateUserClientDto = {
   dni: 'example123',
 };
 
+const userAdminExample: CreateUserAdminDto = {
+  auth_code: String(process.env.AUTH_CODE_CREATE_ADMIN),
+  first_name: 'JohnAdmin2',
+  last_name: 'DoeAdmin2',
+  phone_number: '12345678',
+  email: 'JohnDoeAdmin2@example.ex',
+  username: 'JohnDoeAdmin2',
+  password: '12345678pass',
+  dni: 'example123Admin2',
+};
+
 describe('UsersController', () => {
   let controller: UsersController;
 
@@ -29,16 +41,17 @@ describe('UsersController', () => {
 
     controller = module.get<UsersController>(UsersController);
 
-    // create test user in main database
-    let dbTest = await prisma.user.findUnique({
+    // create test users in main database
+    // client
+    let dbTestClient = await prisma.user.findUnique({
       where: {
         username: 'JohnDoe',
       },
     });
-    if (!dbTest) {
-      dbTest = await prisma.user.create({
+    if (!dbTestClient) {
+      dbTestClient = await prisma.user.create({
         data: {
-          role: 'admin',
+          role: 'client',
           first_name: 'John',
           last_name: 'Doe',
           email: 'John2Doe2@mail.com',
@@ -49,13 +62,34 @@ describe('UsersController', () => {
         },
       });
     }
+
+    // admin
+    let dbTestAdmin = await prisma.user.findUnique({
+      where: {
+        username: 'JohnDoeAdmin',
+      },
+    });
+    if (!dbTestAdmin) {
+      dbTestAdmin = await prisma.user.create({
+        data: {
+          role: 'admin',
+          first_name: 'John',
+          last_name: 'Doe',
+          email: 'John2DoeAdmin@mail.com',
+          username: 'JohnDoeAdmin',
+          password: '123456',
+          dni: '123456789admin',
+          user_state: 'active',
+        },
+      });
+    }
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
 
-  describe('clients/users', () => {
+  describe('users/clients', () => {
     it('should create a user client', async () => {
       const result = await controller.createClient(userClientExample);
       await prisma.user.delete({
@@ -91,6 +125,58 @@ describe('UsersController', () => {
       userClientExample2.email = 'John2Doe2@mail.com';
       try {
         await controller.createClient(userClientExample2);
+      } catch (error) {
+        expect(error).toBeInstanceOf(BadRequestException);
+      }
+    });
+  });
+
+  describe('users/admins', () => {
+    it('should create a user admin', async () => {
+      const result = await controller.createAdmin(userAdminExample);
+      await prisma.user.delete({
+        where: {
+          dni: 'example123Admin2',
+        },
+      });
+      expect(result).toStrictEqual({ message: 'User admin created' });
+    });
+
+    it('should return a unauthorized error if the auth code is wrong', async () => {
+      const userAdminExample2: CreateUserAdminDto = userAdminExample;
+      userAdminExample2.auth_code = 'wrongs';
+      try {
+        await controller.createAdmin(userAdminExample2);
+      } catch (error) {
+        expect(error).toBeInstanceOf(UnauthorizedException);
+      }
+    });
+
+    it('should return an error if the user dni already exists', async () => {
+      const userAdminExample2: CreateUserAdminDto = userAdminExample;
+      userAdminExample2.dni = '123456789admin';
+      try {
+        await controller.createClient(userAdminExample2);
+      } catch (error) {
+        expect(error).toBeInstanceOf(BadRequestException);
+      }
+    });
+
+    it('should return an error if the user username already exists', async () => {
+      const userAdminExample2: CreateUserAdminDto = userAdminExample;
+      userAdminExample2.username = 'JohnDoeAdmin';
+      try {
+        await controller.createClient(userAdminExample2);
+      } catch (error) {
+        expect(error).toBeInstanceOf(BadRequestException);
+      }
+    });
+
+    it('should return an error if the user email already exists', async () => {
+      const userAdminExample2: CreateUserAdminDto = userAdminExample;
+      userAdminExample2.email = 'John2DoeAdmin@mail.com';
+      try {
+        await controller.createClient(userAdminExample2);
       } catch (error) {
         expect(error).toBeInstanceOf(BadRequestException);
       }
